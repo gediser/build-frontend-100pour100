9a61fbac8af4b51af4a50897c1cfd0f59cfba3aa
GEDISER Douala <gediserdouala2021@gmail.com>
2022-06-21 17:43:46 +0200

    Ajout du caroussel slick sur la page d'accueil

--- assets/Dashboard.dd11d05b.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./PageComponent.c8a39b01.js";import{o as e,c as o,w as a,a as s}from"./index.04ad39e7.js";const r=s("Content goes here"),f={setup(c){return(_,n)=>(e(),o(t,{title:"Dashboard"},{default:a(()=>[r]),_:1}))}};export{f as default};
