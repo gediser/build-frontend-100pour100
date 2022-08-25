7b440f403739a3483497d37c8bdd67815d3a9a0b
GEDISER Douala <gediserdouala2021@gmail.com>
2022-08-25 17:19:49 +0200

    Mise a jour de la page publication view

--- assets/Dashboard.64933f23.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./PageComponent.9915b7a2.js";import{o as e,c as o,w as a,a as s}from"./index.e302962f.js";const r=s("Content goes here"),f={setup(c){return(_,n)=>(e(),o(t,{title:"Dashboard"},{default:a(()=>[r]),_:1}))}};export{f as default};
